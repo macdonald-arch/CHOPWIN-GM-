16ad7382c53c932ed8981a45023287e760664068
macdonald-arch <macdonaldchibueze12@gmail.com>
2026-08-16 19:36:41 -0400

    Improve Playwright Slack reporting

--- playwright.config.js
+++ playwright.config.js
@@ -16,7 +16,11 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: 1,
-  reporter: 'html',
+
+  reporter: [
+    ['html', { open: 'never' }],
+    ['json', { outputFile: 'test-results/playwright-results.json' }],
+  ],
 
   use: {
     baseURL: markets[market].baseURL,
